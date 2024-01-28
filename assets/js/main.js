327545f501929d53d430cba16bee2e041d7f2499
Irina Karadimova <irinamaple84@gmail.com>
2024-01-28 20:43:20 +0000

    about

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+    const header = document.getElementById('header');
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SERVICES MODAL ===============*/
 
@@ -7,7 +15,7 @@
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
-/* Link active work */ 
+/* Link active work */
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
@@ -16,7 +24,7 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== LIGHT DARK THEME ===============*/ 
+/*=============== LIGHT DARK THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
