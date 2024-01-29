156123828e65846dafffd5a45b94a5197ca38dca
Irina Karadimova <irinamaple84@gmail.com>
2024-01-29 20:46:37 +0000

    Services and modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,27 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader);
 
 /*=============== SERVICES MODAL ===============*/
+const modalVews = document.querySelectorAll('.services-modal');
+const modalBtns = document.querySelectorAll('.services-button');
+const modalClose = document.querySelectorAll('.services-modal-close');
 
+let modal = function (modalClick) {
+    modalVews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalVews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
