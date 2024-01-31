b95a66263a587e55df6e841916aac74d119307a1
Irina Karadimova <irinamaple84@gmail.com>
2024-01-31 21:51:39 +0000

    testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,25 @@ function activework() {
 linkWork.forEach(l => l.addEventListener('click', activework))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper(".testimonial-container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        },
+    }
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
