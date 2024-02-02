94eac3e15d74e5b2ab0e768a7437aa0f2f7aaeb8
Irina Karadimova <irinamaple84@gmail.com>
2024-02-02 22:52:52 +0000

    footer + scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,8 +74,25 @@ let swiperTestimonial = new Swiper(".testimonial-container", {
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY
 
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 /*=============== LIGHT DARK THEME ===============*/
 
 
